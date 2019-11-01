5d7491bae2dce10bdcd1b9c427242530b71c142b
Brandon Furr <brandon.furr123@gmail.com>
2019-11-01 02:31:20 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+require 'pry'
+class School
+  
+ attr_accessor :name
+ attr_reader :roster
+  
+  @@name = []
+  
+  def initialize(roster)
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    @name = name
+    @grade = grade
+    @@name << name
+    @roster[grade] = @@name
+    
+   end
+  
+ 
+
+    
+ 
+end
+  
+  
